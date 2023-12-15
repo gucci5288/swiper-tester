8ee0f7e03b49836ba886e4bcb7c73d701bc0cb5f
gucciiicool <gucciiicool@dev.com>
2023-12-15 15:52:58 +0800

    swiper11

---
+++ src/components/icons/TestContent.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div class="test-content" >
+    test-content
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+
+
+<style scoped>
+
+.test-content{
+  height: 400px;
+  padding: 16px;
+  background: #2c3e50;
+  border:1px wheat dashed ;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,25 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an swiper page</h1>
+    <swiper
+        :speed="1000"
+    >
+      <swiper-slide
+        v-for="i in 5" :key="i"
+      >
+        <test-content/>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
+
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+
+import TestContent from "@/components/icons/TestContent.vue";
+
+</script>
 
 <style>
 @media (min-width: 1024px) {
